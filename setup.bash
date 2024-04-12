8b62febd058343e688fd524e5e2f8e5c3cbe009c
(Michael) Palassis <40472433+MxNxPx@users.noreply.github.com>
2024-04-12 13:25:46 -0400

    Initial commit

---
+++ setup.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+HELP="
+Usage:
+
+bash $0 UDS_PACKAGE_APP_NAME UDS_PACKAGE_APP_NS [UDS_PACKAGE_APP_HOSTNAME]
+
+UDS_PACKAGE_APP_NAME
+   The name for your new UDS package app.
+
+UDS_PACKAGE_APP_NS
+   The namespace for your new UDS package app.
+
+UDS_PACKAGE_APP_HOSTNAME (optional)
+   The hostname for your new UDS package app to prepend to uds.dev resulting in: "hostname.uds.dev" (defaults to \$UDS_PACKAGE_APP_NAME).
+"
+
+prep_repo() {
+#  echo ${HELP_UDS_PACKAGE_APP_NAME}
+#  echo ${HELP_UDS_PACKAGE_APP_NS}
+#  echo ${HELP_UDS_PACKAGE_APP_HOSTNAME}
+
+  export HELP_UDS_PACKAGE_APP_NAME=${HELP_UDS_PACKAGE_APP_NAME}
+  export HELP_UDS_PACKAGE_APP_NS=${HELP_UDS_PACKAGE_APP_NS}
+  export HELP_UDS_PACKAGE_APP_HOSTNAME=${HELP_UDS_PACKAGE_APP_HOSTNAME}
+
+  find . -type f ! \( -name 'setup.bash' \) -exec sed -i "s|uds-package-app|$HELP_UDS_PACKAGE_APP_NAME|g" '{}' \; -exec ls -ld '{}' \;
+  find . -type f ! \( -name 'setup.bash' \) -exec sed -i "s|uds-package-app-ns|$HELP_UDS_PACKAGE_APP_NS|g" '{}' \; -exec ls -ld '{}' \;
+  find . -type f ! \( -name 'setup.bash' \) -exec sed -i "s|hostname-for-uds-package|$HELP_UDS_PACKAGE_APP_HOSTNAME|g" '{}' \; -exec ls -ld '{}' \;
+}
+
+case "${1:-}" in
+"-h" | "--help" | "help" | "")
+	printf "%s\n" "$HELP"
+	exit 0
+	;;
+*)
+        HELP_UDS_PACKAGE_APP_NAME="$1"
+        HELP_UDS_PACKAGE_APP_NS="$2"
+        HELP_UDS_PACKAGE_APP_HOSTNAME="${3-$HELP_UDS_PACKAGE_APP_NAME}"
+	prep_repo "$@"
+	;;
+esac
